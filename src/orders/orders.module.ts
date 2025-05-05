05f34700ce9c48037b958eb6d84246974de4abed
Sebastian Torres Cabrera <sebastian.torres0426@gmail.com>
2025-05-05 09:55:31 -0500

    feat: change transport to NATS (#5)

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,27 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaService } from '../prisma.service';
-import { MICROSRV_PRODUCT } from '../config/microservices.token';
+import { NatsModule } from '../transports/nats.module';
 
 @Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        name: MICROSRV_PRODUCT,
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
-          options: {
-            host: configService.get('microsrv.product.host'),
-            port: configService.get('microsrv.product.port'),
-          },
-        }),
-        inject: [ConfigService],
-      },
-    ]),
-  ],
+  imports: [NatsModule],
   controllers: [OrdersController],
   providers: [OrdersService, PrismaService],
 })
